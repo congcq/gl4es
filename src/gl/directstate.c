a5f22e448883e397d94f0e76485b8052456b8061
congcq <aiskai2007@gmail.com>
2025-04-24 19:01:29 +0700

    Fix for iOS

--- src/gl/directstate.c
+++ src/gl/directstate.c
@@ -366,7 +366,7 @@ void APIENTRY_GL4ES gl4es_glEnableClientStateIndexed(GLenum array, GLuint index)
         errorShim(GL_INVALID_ENUM);
     }
 }
-AliasDecl(void,gl4es_glEnableClientStatei,(GLenum array, GLuint index),gl4es_glEnableClientStateIndexed);
+AliasDecl(void,gl4es_glEnableClientStatei,(GLenum array, GLuint index),(array, index),gl4es_glEnableClientStateIndexed);
 
 void APIENTRY_GL4ES gl4es_glDisableClientStateIndexed(GLenum array, GLuint index) {
     DBG(printf("glDisableClientStateIndexed(%s, %d)\n", PrintEnum(array), index);)
@@ -380,7 +380,7 @@ void APIENTRY_GL4ES gl4es_glDisableClientStateIndexed(GLenum array, GLuint index
         errorShim(GL_INVALID_ENUM);
     }
 }
-AliasDecl(void,gl4es_glDisableClientStatei,(GLenum array, GLuint index),gl4es_glDisableClientStateIndexed);
+AliasDecl(void,gl4es_glDisableClientStatei,(GLenum array, GLuint index),(array, index),gl4es_glDisableClientStateIndexed);
 
 void APIENTRY_GL4ES gl4es_glEnableVertexArray(GLuint vaobj, GLenum array) {
     DBG(printf("glEnableVertexArray(%d, %s)\n", vaobj, PrintEnum(array));)
